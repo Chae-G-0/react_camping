42e0feabe40b8c6974fd4eaecce7b089b1ce41d8
Chae-G-0 <corkdud@naver.com>
2023-01-18 22:39:52 +0900

    230118

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+// import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=50&pageNo=5&MobileOS=WIN&MobileApp=camipng&serviceKey=ywRlJa7ppqDu3r%2BZgaoE4hxgKL03rb%2FZH6YKSCyaOqRJZa%2B7MMiFJBXuSswp2Hph6Go86ji9%2BmET3T%2BKutJnFg%3D%3D&_type=json"
+      )
+      .then((res) => {
+        console.log(res.data.response.body.items.item);
+      });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>캠핑장 프로젝트</h1>
       </header>
     </div>
   );
