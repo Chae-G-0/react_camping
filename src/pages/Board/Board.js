d607dff5038ee581f799165fb7290ae01a862840
Chae-G-0 <corkdud@naver.com>
2023-03-25 23:59:35 +0900

    board 수정

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -6,19 +6,23 @@ import { useNavigate } from "react-router-dom";
 const Board = () => {
   const navigate = useNavigate();
 
+  const handleWrite = () => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      return navigate("/write");
+    } else {
+      alert("로그인 후 이용할 수 있습니다.");
+      return;
+    }
+  };
+
   return (
     <section className="Board">
       <div className="inner">
         <h2>게시판</h2>
         <BDList />
         <div className="btn">
-          <button
-            onClick={() => {
-              navigate("/write");
-            }}
-          >
-            글쓰기
-          </button>
+          <button onClick={handleWrite}>글쓰기</button>
         </div>
       </div>
     </section>
